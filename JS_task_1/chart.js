7cceca726a6b4a2da9c9cc8c26f5f115bcf1f502
Ablaikhan <aimbetovablay@gmail.com>
2022-09-29 23:38:13 +0600

    1

--- JS_task_1/chart.js
+++ JS_task_1/chart.js
@@ -4,6 +4,7 @@ async function buildPlot() {
     //console.table(data);
     const dateParser = d3.timeParse("%Y-%m-%d");
     const yAccessor = (d) => d.temperatureMin;
+    const yTest = (d) => d.temperatureMax;
     const xAccessor = (d) => dateParser(d.date);
     // Функции для инкапсуляции доступа к колонкам набора данных
 
@@ -26,10 +27,16 @@ async function buildPlot() {
     svg.attr("height",dimension.height);
     svg.attr("width",dimension.width);
     const bounded = svg.append("g");
+    const bounded1 = svg.append("g1");
+
     bounded.style("transform",`translate(${dimension.margin.left}px, ${dimension.margin.top})`);
 
     const yScaler = d3.scaleLinear()
-        .domain(d3.extent(data,yAccessor))
+        .domain(d3.extent(data, yAccessor))
+        .range([dimension.boundedHeight,0]);
+
+    const yScaler1 = d3.scaleLinear()
+        .domain(d3.extent(data, yTest))
         .range([dimension.boundedHeight,0]);
 
     const xScaler = d3.scaleTime()
@@ -40,11 +47,19 @@ async function buildPlot() {
         .x(d => xScaler(xAccessor(d)))
         .y(d => yScaler(yAccessor(d)));
 
+    var lineGenerator1 = d3.line()
+        .x(d => xScaler(xAccessor(d)))
+        .y(d => yScaler1(yTest(d)));
+
     bounded.append("path")
         .attr("d",lineGenerator(data))
-        // .attr("fill","none")
+        .attr("fill","none")
         .attr("stroke","lightgrey")
 
+    bounded.append("path")
+        .attr("d",lineGenerator1(data))
+        .attr("fill","none")
+        .attr("stroke","red")
 }
 
 buildPlot();
